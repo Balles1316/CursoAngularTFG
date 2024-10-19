fd9da39e9c978d9faeccb01d064ccbadc462df6e
ElissaCarr <elissacarr@gmail.com>
2024-10-19 14:02:38 +0200

    add Modules, edit footer and header, fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormularioComponent} from './components/formulario/formulario.component';
 import {ListaTareasComponent} from './components/lista-tareas/lista-tareas.component';
+import {GeneralModule} from './general/general.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormularioComponent, ListaTareasComponent],
+  imports: [RouterOutlet, FormularioComponent, ListaTareasComponent,GeneralModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
